5661e1f0bb5bf59a1cd82a286240d9b428880943
William Tavares <williamtavaresdemoura@gmail.com>
2024-07-28 18:51:07 -0300

    criando novas paginas com php

---
+++ assets/exercicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="css/estilo.css">
+     <title>Curso PHP</title>
+</head>
+<body>
+     <header class="cabecalho">
+          <h1>Curso PHP</h1>
+          <h2>Visualização do Exercício</h2>
+     </header>
+     <main class="princial">
+          <div class="conteudo">
+          </div>
+     </main>
+     <footer class="rodape">
+          COD3R & ALUNOS ₢ <?= date('Y') ?>
+     </footer>
+</body>
+</html>
