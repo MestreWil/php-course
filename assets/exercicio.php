f62a696f9266fc7b7231da93a0bb618ba789e3ef
William Tavares <williamtavaresdemoura@gmail.com>
2024-07-30 23:48:08 -0300

    criando navegacao

--- assets/exercicio.php
+++ assets/exercicio.php
@@ -5,13 +5,18 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="css/estilo.css">
+     <link rel="stylesheet" href="css/exercicio.css">
      <title>Curso PHP</title>
 </head>
-<body>
+<body class="exercicio">
      <header class="cabecalho">
           <h1>Curso PHP</h1>
           <h2>Visualização do Exercício</h2>
      </header>
+     <nav class="navegacao">
+          <a href="#" class="verde">Sem formatação</a>
+          <a href="index.php" class="vermelho">Voltar</a>
+     </nav>
      <main class="princial">
           <div class="conteudo">
           </div>
