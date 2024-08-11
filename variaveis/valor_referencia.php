7a37748cd1a1bb55019154c003afa6dc9d35aec7
William Tavares <williamtavaresdemoura@gmail.com>
2024-08-11 01:57:29 -0300

    finalizando o modulo de variaveis

---
+++ variaveis/valor_referencia.php
@@ -0,0 +1,20 @@
+<div class="titulo">Valor vs Referência</div>
+
+<?php
+
+$variavel = 'valor inicial';
+
+echo $variavel;
+
+// Atribuição por valor
+$variavelValor = $variavel;
+
+echo "<br> $variavelValor";
+$variavelValor = 'novo valor';
+echo "<br> $variavelValor";
+
+// Atribuição por referência
+$variavelReferencia = &$variavel;
+$variavelReferencia = "mesma referencia";
+
+echo "<br> $variavel | $variavelReferencia";
