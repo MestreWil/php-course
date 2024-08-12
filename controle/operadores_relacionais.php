6d1bce040bb7d315e28357ca6e886899c2184c35
William Tavares <williamtavaresdemoura@gmail.com>
2024-08-11 22:51:51 -0300

    spaceship

--- controle/operadores_relacionais.php
+++ controle/operadores_relacionais.php
@@ -1,5 +1,11 @@
 <div class="titulo">Operadores Relacionais</div>
 
+<style type="text/css">
+     p {
+          margin-bottom: -10px;
+     }
+</style>
+
 <?php
 var_dump(1 == 1); //igual *true
 var_dump(1 > 1); // maior *false
@@ -14,13 +20,18 @@ var_dump(1 ==='1'); // com três simbolos de igual ele compara a equivalencia e 
 var_dump( 1 != '1'); // ve a diferenca equivalente *false
 var_dump(1 !== '1'); // ve a diferenca equivalente e dos tipos *true
 
-echo "<p>Relacioanaisno If/Else</p><hr>";
+echo "<p>Relacionamento no If/Else</p><hr>";
 
 $idade = 25;
 if ($idade < 18) {
      echo "menor de Idade = $idade anos<br>";
 } else if ($idade <= 65) {
-     echo "Adulto = $idade anos<br>"
+     echo "Adulto = $idade anos<br>";
 } else {
      echo "Terceira idade! = $idade anos";
 }
+
+echo '<p>Spaceship</p><hr>';
+var_dump(5 <=> 3); // saber se um numero é maior que outro *retorna 1 se o numero a esquerda for maior que o da direita
+var_dump(50 <=> 50); // *retorna 0 se os numeros forem iguais
+var_dump( 5 <=> 50);// *retorna -1 se o numero da direita for maior que o da esquerda
