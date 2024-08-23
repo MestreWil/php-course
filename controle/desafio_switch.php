09884692a902681f7386680f91e635fb94e4da36
ABC <williamtavaresdemoura@gmail.com>
2024-08-23 18:12:12 -0300

    Update desafio_switch.php
    aprendendo switch

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -22,22 +22,62 @@
 
 <?php
 
+const FATOR_KM_MILHA = 0.621371;
+const FATOR_METRO_KM = 1000;
 
-switch ($_POST['conversao']){
+$parametro = $_POST['param'] ?? 0;
+
+switch($_POST['conversao']){
+    case 'km-milha':
+        $distancia = $parametro * FATOR_KM_MILHA;
+        $msg = "$parametro Km = $distancia Milhas";
+        break;
+
+    case 'milha-km':
+        $distancia = $parametro / FATOR_KM_MILHA;
+        $msg = "$parametro Milhas = $distancia km";
+        break;
+    
     case 'metro-km':
-        $resultado = $_POST['param']/1000;
-        echo "$_POST[param] equivale a $resultado Km";
+        $distancia = $paremetro * FATOR_METRO_KM;
+        $msg = "$paremetro Metros = $distancia km";
         break;
     case 'km-metro':
-        $resultado = $_POST['param']*1000;
-        echo "$_POST[param] equivale a $resultado M";
+        $distancia = $parametro / FATOR_METRO_KM;
+        $msg = "$parametro Km = $distancia metros";
         break;
-    case 'km-milha':
-        $resultado = $_POST['param']/1.6;
-        echo "$_POST[param] equivale a $resultado Milhas";
-        break;
-    case 'km-milha':
-        $resultado = $_POST['param']*1.6;
-        echo "$_POST[param] equivale a $resultado Km";
-        break;
+    
+    default:
+        $mensagem = "Nenhum valor calculado";
+    
 }
+echo "<br>$msg<br>";
+
+
+
+
+
+
+
+
+
+// Verificacao para caso o parametro nao seja setado ele atribua o valor 0 a ele
+
+// switch ($_POST['conversao']){
+//     case 'metro-km':
+//         $resultado = $_POST['param']/1000;
+//         echo "$_POST[param] equivale a $resultado Km";
+//         break;
+//     case 'km-metro':
+//         $resultado = $_POST['param']*1000;
+//         echo "$_POST[param] equivale a $resultado M";
+//         break;
+//     case 'km-milha':
+//         $resultado = $_POST['param']/1.6;
+//         echo "$_POST[param] equivale a $resultado Milhas";
+//         break;
+//     case 'km-milha':
+//         $resultado = $_POST['param']*1.6;
+//         echo "$_POST[param] equivale a $resultado Km";
+//         break;
+// }
