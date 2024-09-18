ea11de2bb19e53834a4efcb5051ad4157b866e65
ABC <williamtavaresdemoura@gmail.com>
2024-09-18 17:41:07 -0300

    Update operacoes.php

--- array/operacoes.php
+++ array/operacoes.php
@@ -14,6 +14,8 @@ $dados2 = [
 $dadosCompletos = $dados1 + $dados2;
 print_r($dadosCompletos);
 
+$dadosCompletos["endereco"] = "Rua C, Quadra 5, N39";
+
 echo '<br>' . is_array($dadosCompletos);
 echo '<br>' . count($dadosCompletos);
 
@@ -29,3 +31,18 @@ echo '<br>';
 unset($dadosCompletos);
 echo "<br>";
 echo "{$dadosCompletos}";
+
+$impares = [1, 3, 5, 7, 9];
+$pares = [0, 2, 4, 6, 8];
+
+$decimais = $pares + $impares;
+echo "<br>";
+print_r($decimais);
+// junta os arrays de forma segura
+$novo_decimais = array_merge($pares, $impares);
+echo "<br>";
+print_r($novo_decimais);
+
+echo "<br>";
+// a funcao sort altera o array para deixalo na ordem
+print_r(sort($novo_decimais));
