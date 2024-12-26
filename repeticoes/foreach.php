4d1ee6c64377a56c4dca4f9430b9c89d507d4f82
ABC <williamtavaresdemoura@gmail.com>
2024-12-25 21:10:24 -0300

    Update foreach.php

--- repeticoes/foreach.php
+++ repeticoes/foreach.php
@@ -1,1 +1,41 @@
 <div class="titulo">For Each</div>
+
+<?php
+$array = [
+    1 => 'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta', 
+    'Quinta',
+    'Sexta',
+    'Sábado'
+];
+
+foreach ($array as $valor){
+    echo "$valor <br>";
+}
+foreach ($array as $indice => $valor){
+    echo "$indice => $valor <br>";
+}
+
+$matrix = [
+    ['a', 'e', 'i', 'o', 'u'],
+    ['b', 'c', 'd']
+];
+foreach($matrix as $linha){
+    //echo "$linha <br>"; 
+    foreach($linha as $valor){
+        echo "$valor";
+    }
+    echo "<br>";
+}
+
+// É possivel manipular e mudar o array com foreach, o & é para apontar para a referencia de memoria do array
+// sem o & o array permanece o mesmo
+$numeros = [1, 2, 3, 4, 5, 6];
+
+foreach ($numeros as &$dobrar){
+    $dobrar *= 2; 
+    echo "$dobrar <br>";
+}
+print_r($numeros);
