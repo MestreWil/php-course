e5c2278b3d08824fc1faebc5b77a2f71329751ad
ABC <williamtavaresdemoura@gmail.com>
2024-12-26 02:24:03 -0300

    Create desafio_impressao.php

---
+++ repeticoes/desafio_impressao.php
@@ -0,0 +1,29 @@
+<div class="titulo">Desafio Impressão</div>
+
+<!--
+Enunciado:
+    -  Imprima apenas os valores do array  que contém indice par
+    - Desafio adicional: Resolver com For e Foreach 
+    - Valores esperados: AAA, CCC, EEE
+-->
+
+<?php
+
+$array = [
+    "AAA",
+    "BBB",
+    "CCC",
+    "DDD",
+    "EEE",
+    "FFF"
+];
+
+for($i = 0; $i < count($array); $i++){
+    if($i % 2 === 1) continue;
+    echo "$array[$i] <br>";
+}
+echo "<hr>";
+foreach($array as $indice => $valor){
+    if($indice % 2 === 1) continue; 
+    echo "$valor <br>";
+}
