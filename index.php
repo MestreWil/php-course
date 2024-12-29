d6a2389ae2564045868bf6da034649a30762ee63
ABC <williamtavaresdemoura@gmail.com>
2024-12-29 15:55:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -297,6 +297,11 @@
                                         Argumentos Variáveis
                                    </a>
                               </li>
+                              <li>
+                                   <a href="exercicio.php?dir=funcoes&file=args_padrao">
+                                        Argumentos Padrão
+                                   </a>
+                              </li>
                          </ul>
                     </div>
                </nav>
