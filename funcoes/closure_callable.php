53ea24ac729c6e6cfc74f0d50494dee6eb6e3ca4
ABC <williamtavaresdemoura@gmail.com>
2024-12-29 20:20:23 -0300

    closure callable

---
+++ funcoes/closure_callable.php
@@ -0,0 +1,24 @@
+<div class="titulo">Closure & Callable</div>
+
+<?php
+
+$soma1 = function($a, $b){
+    return $a + $b;
+};
+
+echo $soma1(2, 3) ;
+echo (is_callable($soma1) ? 'Sim' : 'Não') . "<br>";
+
+
+function soma2($a = null, $b = null){
+    return $a + $b;
+}
+
+// Closure, classe que representa uma funcao anonima, o php interpreta uma funcao anonima como uma classe clossure
+// Ja calleble é uma funcao passivel de ser chamada(invocada), na funcao anonima ela é callable, mas na funcao normal, 
+// hoje ela não é callable
+echo soma2(2, 3);
+
+echo (is_callable(soma2()) ? 'Sim' : 'Não') . "<br>";
+
+var_dump($soma1);
--- index.php
+++ index.php
@@ -312,6 +312,11 @@
                                         Funções Anonimas
                                    </a>
                               </li>
+                              <li>
+                                   <a href="exercicio.php?dir=funcoes&file=closure_callable">
+                                        Closure & Callable
+                                   </a>
+                              </li>
                          </ul>
                     </div>
                </nav>
