909abbc25cbc1770417f6b7c0f38c0003d6b34db
ABC <williamtavaresdemoura@gmail.com>
2025-02-13 17:37:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -402,6 +402,11 @@
                                         Traits 02
                                    </a>
                               </li>
+                              <li>
+                                   <a href="exercicio.php?dir=classes_objetos&file=magic_methods">
+                                        Métodos Mágicos
+                                   </a>
+                              </li>
                          </ul>
                     </div>
                </nav>
