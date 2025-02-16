0e82d852c1959c759193870e1430c3333774fec6
ABC <williamtavaresdemoura@gmail.com>
2025-02-16 03:18:03 -0300

    Create include_once_arquivo.php

---
+++ includes/include_once_arquivo.php
@@ -0,0 +1,7 @@
+<div class="titlo">Include Once</div>
+
+<?php
+echo "Carregando: include_once_arquivo<br>";
+$variavel = 'Estou definida';
+// o include e o require once é pra ser usado quando vc quer que o arquivo seja chamado uma unica vez
+// ele verifica se o arquivo ja foi carregado no codigo, e se foi, nao o executaS
