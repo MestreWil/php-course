aa7efeffd6de2b125ab749ebfd365cff731f633f
William Tavares <williamtavaresdemoura@gmail.com>
2025-05-23 16:00:45 -0300

    Create datas_01.php

---
+++ api/datas_01.php
@@ -0,0 +1,5 @@
+<div class="titulo">Datas #01</div>
+<?php
+echo time() . '<br>';
+echo date('D, d \d\e M \d\e Y H:i A') . '<br>';
+// usando o setlocale eu posso determinar de onde o date e o time vão pegar as informações de hora, data e etc
